95c25066c2c6057a8307553940b6b31ce58e22c5
Athirson <athirsonarceus@gmail.com>
2022-12-27 15:26:12 -0300

    first commit

---
+++ routes/index.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+	return (
+		<>
+			<div class={'w-screen h-screen bg-gray-900'}>
+				<div class='p-8 mx-auto max-w-screen-md bg-gray-900'>
+					<img
+						src='/Bitcoin.png'
+						width={'200px'}
+						class='w-32 h-32 mx-auto'
+						alt='the fresh logo: a sliced lemon dripping with juice'
+					/>
+					<p class='my-10 text(center 3xl white)'>
+						Bitcoin price tracker
+					</p>
+				</div>
+			</div>
+		</>
+	)
+}
